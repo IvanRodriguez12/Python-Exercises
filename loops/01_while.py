332ac31e6159ffffa4594a4bf04998821bf5fa47
desa <ivanrosriguez7@gmail.com>
2026-03-02 19:21:24 -0300

    Loops

---
+++ loops/01_while.py
@@ -0,0 +1,14 @@
+# bucles while
+
+contador = 0
+
+#while contador <= 5:
+#    print(contador)
+#    contador += 1
+
+# Bucle while infinito se corta con break cuando cumple una condicion
+while True:
+    print(contador)
+    contador+=1
+    if contador == 5:
+        break
