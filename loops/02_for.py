68f678ed13c72c78c4001f35d9cdf107a61e53d5
desa <ivanrosriguez7@gmail.com>
2026-03-05 11:17:02 -0300

    For Examples

--- loops/02_for.py
+++ loops/02_for.py
@@ -10,3 +10,41 @@ londrea = "Londres"
 
 for letra in londrea:
     print(letra)
+
+
+# Ejemplo de recuperar el indice enumerated()
+cosas = ["Libro", "Gas", "Cartuchera"]
+
+for index, cosa in enumerate(cosas):
+    print(f"El indice de {cosa} es {index}")
+
+
+# For anidados
+print("\nEjemplo de For anidado: ")
+letras = ["A", "B", "C"]
+numeros = [1,2,3,4,5]
+
+for letra in letras:
+    for numero in numeros:
+        print(f"{letra}{numero}")
+
+# RECOMENDACION PYTHON TUTOR (Te muestra que hace cada linea de tu codigo)
+print("\nBreak: ")
+animales = ["Perro", "Canario", "Gato", "Elefante"]
+
+for animal in animales:
+    if animal == "Gato":
+        print("Se encontro al Gato")
+        break
+    print(animal)
+
+print("\nContinue: ")
+
+for animal in animales:
+    if animal == "Canario":
+        continue
+    print(animal)
+
+# Comprension de listas (Listas comprehesion)
+animales_mayus = [animal.upper() for animal in animales]
+print(animales_mayus)
