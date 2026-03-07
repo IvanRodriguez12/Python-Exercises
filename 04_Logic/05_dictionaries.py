ee7361a6341d5526d69b1f01099c43041bec4b09
desa <ivanrosriguez7@gmail.com>
2026-03-07 20:30:18 -0300

    Dictionaries

---
+++ 04_Logic/05_dictionaries.py
@@ -0,0 +1,46 @@
+# Diccionarios, son colecciones de clave-valor y sirven para guardar datos relacionados
+persona = {
+    "nombre": "Juan",
+    "edad": 20,
+    "sexo": "hombre",
+    "running_in_the_90s": True,
+    "calificaciones": [5,8,9],
+    "redes_sociales": {
+        "whatsapp": True
+    }
+}
+
+# Acceder a uno de los campos
+print(persona["nombre"])
+print(persona["redes_sociales"]["whatsapp"])
+print(persona["sexo"])
+
+# Eliminar una clave-valor
+del persona["edad"]
+print(persona)
+
+# Cambiar un valor
+persona["Sexo"] = "Mujer"
+persona["calificaciones"][1] = 2 
+
+# Eliminar un campo y devolverlo
+is_running = persona.pop("running_in_the_90s")
+print(is_running)
+print(persona)
+
+
+# Sobreescribir los valores de un diccionario
+a = {"nombre": "juan Carlos", "edad": 20, "tiene_licencia": True, "embarazada": False}
+b = {"nombre": "juan ", "edad": 20, "tiene_licencia": False, "Tiene_hijos": True}
+
+
+print("\n")
+print(a)
+print(b)
+
+a.update(b)
+print(a)
+
+# Verificar que un diccionario tenga ese campo
+print("nombre" in persona)
+print("religion" in persona)
