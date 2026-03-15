c1d43c075fb3dbc3035f4b6f8912dfc0246d9c4c
desa <ivanrosriguez7@gmail.com>
2026-03-15 18:36:45 -0300

    RegEx Exercises

--- 05_regex/01_re.py
+++ 05_regex/01_re.py
@@ -41,7 +41,7 @@ print(result.end())
 # e indica en que posición empieza y termina la coincidencia.
 text = "Todo el mundo dice que la IA nos va a quitar el trabajo. Pero solo hace falta ver como la puede cagar con las Regex para ir con cuidado"
 pattern = "IA"
-encontrar_patron = re.search(pattern, text)
+encontrar_patron = re.search(pattern, text, re.IGNORECASE)
 
 if encontrar_patron:
     print(f"Encontramos el patron en {encontrar_patron.start()} y terminaba en {encontrar_patron.end()}")
@@ -56,4 +56,46 @@ pattern = "Pytho."
 matches = re.findall(pattern, text)
 print(matches)
 
-# ME QUEDE EN LAS 5 HORAS
+# .iter() devuelve un iterador con las coincidencias encontradas
+matches_iter = re.finditer(pattern, text)
+for match in matches_iter:
+    print(match.group(), match.start(), match.end())
+
+# EJERCICIO 02
+# Encuentra todas las ocurrencias de la palabra "midu" en el siguiente texto e indica en que posición empieza y termina cada coincidencia y cuantas veces se encontró.
+text = "Este es el curso de Python de midudev. ¡Suscríbete a midudev si te gusta este contenido! midu"
+
+### Modificadores
+
+# Los modificadores son opciones que se pueden agregar a un patrón para cambiar su comportamiento
+
+# re.IGNORECASE: Ignora las mayúsculas y minúsculas
+
+pattern = "midu"
+
+matches = re.findall(pattern, text)
+
+matches_iter = re.finditer(pattern, text)
+for match in matches_iter:
+    print(match.group(), match.start(), match.end())
+
+#regular expressions 111
+
+# EJERCICIO 03
+# Encuentra todas las ocurrencias de la palabra "python" en el siguiente texto, sin distinguir entre mayúsculas y minúsculas.
+text = "Este es el curso de Python de midudev. ¡Suscríbete a python si te gusta este contenido! PYTHON"
+
+pattern = "Python"
+
+encontrar_patron = re.findall(pattern, text, re.IGNORECASE)
+print(encontrar_patron)
+print(len(encontrar_patron))
+
+
+# .sub() reemplaza todas las coincidencias de un patrón en un texto
+text = "Hola mundo jaja si, hola a todos"
+pattern = "Hola"
+replacement = "Adios"
+
+reemplazar_patron = re.sub(pattern, replacement, text,count=0)
+print(reemplazar_patron)
